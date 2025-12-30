cf3f7223ffb46cd8f51e4abecf7637b8be1cd270
Nikhil Satya <bnikhilsatya@gmail.com>
2025-12-30 20:33:12 +0530

    Update app.py
    nothing done

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ st.set_page_config(
     page_icon="fevicon_project.png",
     layout="wide"
 )
-
 # -------------------------------------------------
 # SESSION STATE
 # -------------------------------------------------
